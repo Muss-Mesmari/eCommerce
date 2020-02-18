a5cfc7bd5a396dbf89686f4c815cd9a917228e45
M. Messmari <mussmesmari.developer@gmail.com>
2020-02-18 22:09:47 +0100

    Homepage, shared views added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.IRepository;
+using eCommerce.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(IProductRepository pieRepository)
+        {
+            _productRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                ProductsOfTheWeek = _productRepository.ProductsOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using eCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Product> ProductsOfTheWeek { get; set; }
+    }
+}
